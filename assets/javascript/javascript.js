752134fdda59e41ce1ae11972aa002cb8d8176d8
Wesley Hanson <wrh1600@gmail.com>
2018-03-19 23:13:03 -0700

    added projects/styling/js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -13,13 +13,46 @@ $('.gifImg').click(function(){
 $('.rpsImg').click(function(){
 	$('.rpsInfo').slideToggle(500);
 })
+
+
+$('#aboutArrow').click(function(){
+	$('#aboutArrowUp').show('fast');
+	$('#aboutMe').css('visibility', 'visible');
+	$('#aboutMe').addClass('animated fadeInLeftBig');
+	$('#aboutMe').removeClass('fadeOutLeftBig');
+	$('#aboutArrow').hide('fast');
+})
+$('#aboutArrowUp').click(function(){
+	$('#aboutArrow').show('fast');
+	$('#aboutArrowUp').hide('fast');
+	$('#aboutMe').addClass('animated fadeOutLeftBig');
+	$('#aboutMe').removeClass('fadeInLeftBig');
+	setTimeout(function(){
+		$('#aboutMe').css('visibility', 'hidden');
+	}, 1000);
+})
+
+
 	
 $('#worksArrow').click(function(){
-	$('#projects').slideToggle(500);
+	$('#worksArrowUp').show('fast');
+	$('#projects').css('visibility', 'visible');
+	$('#projects').addClass('animated fadeInDownBig');
+	$('#projects').removeClass('fadeOutUpBig');
+	$('#worksArrow').hide('fast');
 })
-$('#contactArrow').click(function(){
-	$('#contactMe').slideToggle(500);
+$('#worksArrowUp').click(function(){
+	$('#worksArrow').show('fast');
+	$('#worksArrowUp').hide('fast');
+	$('#projects').addClass('animated fadeOutUpBig');
+	$('#projects').removeClass('fadeInDownBig');
+	setTimeout(function(){
+		$('#projects').css('visibility', 'hidden');
+	}, 1000);
 })
-$('#aboutArrow').click(function(){
-	$('#aboutMe').slideToggle(500);
+
+$('.projectPic').hover(function(){
+	$(this).addClass('animated infinite pulse');
+}, function(){
+	$(this).removeClass('animated infinite pulse')
 })
