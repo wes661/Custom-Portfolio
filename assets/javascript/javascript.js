c94f33751a0215939eececb58dcde114e6ad28c1
Wesley Hanson <wrh1600@gmail.com>
2018-03-20 21:19:14 -0700

    css and js changes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,18 +1,4 @@
-$('.rpgImg').click(function(){
-	$('.rpgInfo').slideToggle(500);
-})
-$('.triviaImg').click(function(){
-	$('.triviaInfo').slideToggle(500);
-})
-$('.hangmanImg').click(function(){
-	$('.hangmanInfo').slideToggle(500);
-})
-$('.gifImg').click(function(){
-	$('.gifInfo').slideToggle(500);
-})
-$('.rpsImg').click(function(){
-	$('.rpsInfo').slideToggle(500);
-})
+
 
 
 $('#aboutArrow').click(function(){
