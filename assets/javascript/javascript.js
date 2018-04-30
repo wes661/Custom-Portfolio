40fb2d8267f74c3ae59e2a03d13cf78a536384ce
Wesley Hanson <wrh1600@gmail.com>
2018-04-29 22:34:15 -0700

    updated profile, added jquery nav and more style

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,15 +1,31 @@
 
-$(".mail").hover(function(){
-	$(this).addClass('animated infinite pulse');
-},
-function(){
-	$(this).removeClass('animated infinite pulse')
-})
-$(".projectPic").hover(function(){
-	$(this).addClass('animated infinite pulse');
-},
-function(){
-	$(this).removeClass('animated infinite pulse')
+// $(".mail").hover(function(){
+// 	$(this).addClass('animated pulse');
+// },
+// function(){
+// 	$(this).removeClass('animated pulse')
+// })
+// $(".projectPic").hover(function(){
+// 	$(this).addClass('animated pulse');
+// },
+// function(){
+// 	$(this).removeClass('animated pulse')
+// })
+
+$('#portfolio-btn').click(function(){
+	$('.nav').removeClass('animated fadeOutLeftBig');
+	$('.nav').css('visibility', 'visible');
+	$('.nav').addClass('animated fadeInLeftBig');
+	$('#portfolio-btn').addClass('animated fadeOutUp');
+	$('#portfolio-back-btn').css('visibility', 'visible');
+	$('#portfolio-back-btn').removeClass('animated fadeOutUp');
+	$('#portfolio-back-btn').addClass('animated fadeInDown');
+});
+$('#portfolio-back-btn').click(function(){
+	$('.nav').addClass('animated fadeOutLeftBig');
+	$('#portfolio-back-btn').addClass('animated fadeOutUp');
+	$('#portfolio-btn').removeClass('animated fadeOutUp');
+	$('#portfolio-btn').addClass('animated fadeInDown');
 })
 
 $('#aboutArrow').click(function(){
