7e8334fc48d7fd6772f603fc5b7fd626b8394081
Wesley Hanson <wrh1600@gmail.com>
2018-05-16 20:29:32 -0700

    new portfolio pic

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,17 +1,4 @@
 
-// $(".mail").hover(function(){
-// 	$(this).addClass('animated pulse');
-// },
-// function(){
-// 	$(this).removeClass('animated pulse')
-// })
-// $(".projectPic").hover(function(){
-// 	$(this).addClass('animated pulse');
-// },
-// function(){
-// 	$(this).removeClass('animated pulse')
-// })
-
 $('#portfolio-btn').click(function(){
 	$('.nav').removeClass('animated fadeOutLeftBig');
 	$('.nav').css('visibility', 'visible');
@@ -28,56 +15,4 @@ $('#portfolio-back-btn').click(function(){
 	$('#portfolio-btn').addClass('animated fadeInDown');
 })
 
-$('#aboutArrow').click(function(){
-	$('#aboutArrowUp').show('fast');
-	$('#aboutMe').css('visibility', 'visible');
-	$('#aboutMe').addClass('animated fadeInLeftBig');
-	$('#aboutMe').removeClass('fadeOutLeftBig');
-	$('#aboutArrow').hide('fast');
-})
-$('#aboutArrowUp').click(function(){
-	$('#aboutArrow').show('fast');
-	$('#aboutArrowUp').hide('fast');
-	$('#aboutMe').addClass('animated fadeOutLeftBig');
-	$('#aboutMe').removeClass('fadeInLeftBig');
-	setTimeout(function(){
-		$('#aboutMe').css('visibility', 'hidden');
-	}, 1000);
-})
-
-$('#aboutArrow').click(function(){
-	$('#aboutArrowUp').show('fast');
-	$('#aboutMe').css('visibility', 'visible');
-	$('#aboutMe').addClass('animated fadeInLeftBig');
-	$('#aboutMe').removeClass('fadeOutLeftBig');
-	$('#aboutArrow').hide('fast');
-})
-$('#aboutArrowUp').click(function(){
-	$('#aboutArrow').show('fast');
-	$('#aboutArrowUp').hide('fast');
-	$('#aboutMe').addClass('animated fadeOutLeftBig');
-	$('#aboutMe').removeClass('fadeInLeftBig');
-	setTimeout(function(){
-		$('#aboutMe').css('visibility', 'hidden');
-	}, 1000);
-})
-
-
-	
-$('#worksArrow').click(function(){
-	$('#worksArrowUp').show('fast');
-	$('#projects').css('visibility', 'visible');
-	$('#projects').addClass('animated fadeInDownBig');
-	$('#projects').removeClass('fadeOutUpBig');
-	$('#worksArrow').hide('fast');
-})
-$('#worksArrowUp').click(function(){
-	$('#worksArrow').show('fast');
-	$('#worksArrowUp').hide('fast');
-	$('#projects').addClass('animated fadeOutUpBig');
-	$('#projects').removeClass('fadeInDownBig');
-	setTimeout(function(){
-		$('#projects').css('visibility', 'hidden');
-	}, 1000);
-})
 
