cc56d193c515b26c01c646e9798ad7538a52fd0c
Wesley Hanson <wrh1600@gmail.com>
2018-06-04 20:57:48 -0700

    added scroll animations

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -15,4 +15,19 @@ $('#portfolio-back-btn').click(function(){
 	$('#portfolio-btn').addClass('animated fadeInDown');
 })
 
+jQuery(document).ready(function(){
+	jQuery('.postOne').addClass('hidden').viewportChecker({
+		classToAdd: 'visible animated fadeInLeft',
+		offset: 300
+	});
+});	
+
+jQuery(document).ready(function(){
+	jQuery('.postTwo').addClass('hidden').viewportChecker({
+		classToAdd: 'visible animated fadeInRight',
+		offset: 300
+	});
+});	
+
+
 
