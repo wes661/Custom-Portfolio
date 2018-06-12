deee2bf123142578a35baf2454deca12507acc64
Wesley Hanson <wrh1600@gmail.com>
2018-06-11 22:41:50 -0700

    added footer and react project

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,14 +19,14 @@ $('#portfolio-back-btn').click(function(){
 jQuery(document).ready(function(){
 	jQuery('.postOne').addClass('hidden').viewportChecker({
 		classToAdd: 'visible animated fadeInLeft',
-		offset: 500
+		offset: 300
 	});
 });	
 
 jQuery(document).ready(function(){
 	jQuery('.postTwo').addClass('hidden').viewportChecker({
 		classToAdd: 'visible animated fadeInRight',
-		offset: 500
+		offset: 300
 	});
 });	
 
