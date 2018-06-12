f84374a3c060c9a8c84ecb43858750020d32f307
Wesley Hanson <wrh1600@gmail.com>
2018-06-11 21:59:13 -0700

    more styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -19,14 +19,14 @@ $('#portfolio-back-btn').click(function(){
 jQuery(document).ready(function(){
 	jQuery('.postOne').addClass('hidden').viewportChecker({
 		classToAdd: 'visible animated fadeInLeft',
-		offset: 300
+		offset: 500
 	});
 });	
 
 jQuery(document).ready(function(){
 	jQuery('.postTwo').addClass('hidden').viewportChecker({
 		classToAdd: 'visible animated fadeInRight',
-		offset: 300
+		offset: 500
 	});
 });	
 
